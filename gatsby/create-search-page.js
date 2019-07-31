a908cad42de1e8bec80f5e173ae70ec7e841b0a3
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-31 14:37:22 -0700

    implement 'filter by category'

--- gatsby/create-search-page.js
+++ gatsby/create-search-page.js
@@ -5,54 +5,52 @@ const path = require('path');
 module.exports = async (graphql, actions) => {
   const { createPage } = actions;
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        filter: {
-          frontmatter: {
-            template: { eq: "post" }
-            draft: { ne: true }
-            language: { eq: "en" }
-          }
-        }
-      ) {
-        totalCount
-      }
-    }
-  `);
+  // const result = await graphql(`
+  //   {
+  //     allMarkdownRemark(
+  //       filter: {
+  //         frontmatter: {
+  //           template: { eq: "post" }
+  //           draft: { ne: true }
+  //           language: { eq: "en" }
+  //         }
+  //       }
+  //     ) {
+  //       totalCount
+  //     }
+  //   }
+  // `);
 
   createPage({
     path: '/search',
     component: path.resolve('./src/templates/search-template.js'),
     context: {
-      language: 'en',
-      totalCount: result.data.allMarkdownRemark.totalCount
+      language: 'en'
     }
   });
 
   // ja
-  const resultJa = await graphql(`
-    {
-      allMarkdownRemark(
-        filter: {
-          frontmatter: {
-            template: { eq: "post" }
-            draft: { ne: true }
-            language: { eq: "ja" }
-          }
-        }
-      ) {
-        totalCount
-      }
-    }
-  `);
+  // const resultJa = await graphql(`
+  //   {
+  //     allMarkdownRemark(
+  //       filter: {
+  //         frontmatter: {
+  //           template: { eq: "post" }
+  //           draft: { ne: true }
+  //           language: { eq: "ja" }
+  //         }
+  //       }
+  //     ) {
+  //       totalCount
+  //     }
+  //   }
+  // `);
 
   createPage({
     path: '/search/ja',
     component: path.resolve('./src/templates/search-template.js'),
     context: {
-      language: 'ja',
-      totalCount: resultJa.data.allMarkdownRemark.totalCount
+      language: 'ja'
     }
   });
 };
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import type { Edges } from '../../types';
 import BlogList from './BlogList';
 import styles from './Search.module.scss';
@@ -12,18 +12,54 @@ type Props = {
 const Search = ({ edges, totalCount, language }: Props) => {
   const categoriesList =
     language === 'ja' ? useCategoriesListJa() : useCategoriesList();
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [tags, setTags] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [number, setNumber] = useState([]);
+
+  useEffect(() => {
+    setBlogs(edges);
+    setNumber(totalCount);
+  }, []);
+
+  const onClickCategory = category => {
+    if (category === selectedCategory) {
+      setSelectedCategory('');
+      setBlogs(edges);
+      setNumber(totalCount);
+    } else {
+      setSelectedCategory(category);
+      filterBlogByCategory(category);
+    }
+  };
+
+  const filterBlogByCategory = category => {
+    let blogs = edges.filter(
+      edge => edge.node.frontmatter.category === category
+    );
+    setNumber(blogs.length);
+    return setBlogs(blogs);
+  };
+
   return (
     <div className={styles['search']}>
       <h1>{language === 'en' ? 'Search' : 'さがす'}</h1>
       <p>
-        {language === 'en' ? 'blog' : 'ブログ'}: {totalCount}
+        {language === 'en' ? 'blog' : 'ブログ'}: {number}
       </p>
       <ul>
         {categoriesList.map(category => {
-          return <li key={category.fieldValue}>{category.fieldValue}</li>;
+          return (
+            <li
+              onClick={() => onClickCategory(category.fieldValue)}
+              key={category.fieldValue}
+            >
+              {category.fieldValue}
+            </li>
+          );
         })}
       </ul>
-      <BlogList edges={edges} />
+      <BlogList edges={blogs} />
     </div>
   );
 };
