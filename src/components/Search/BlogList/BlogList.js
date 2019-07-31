09bc46080ee35215ead5bb21ff7cb637b861254a
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-31 13:24:21 -0700

    create BlogList component under Search component

---
+++ src/components/Search/BlogList/BlogList.js
@@ -0,0 +1,52 @@
+// @flow
+import React from 'react';
+import moment from 'moment';
+import { Link } from 'gatsby';
+import type { Edges } from '../../../types';
+import styles from './BlogList.module.scss';
+
+type Props = {
+  edges: Edges
+};
+
+const BlogList = ({ edges }: Props) => {
+  return (
+    <div className={styles['blog-list']}>
+      {edges.map(edge => {
+        return (
+          <Link
+            className={styles['blog-list__item-title-link']}
+            key={edge.node.fields.slug}
+            to={edge.node.fields.slug}
+          >
+            <div className={styles['blog-list__item']}>
+              <div className={styles['blog-list__item-meta']}>
+                <time
+                  className={styles['blog-list__item-meta-time']}
+                  dateTime={moment(edge.node.frontmatter.date).format(
+                    'MMMM D, YYYY'
+                  )}
+                >
+                  {moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}
+                </time>
+                <span className={styles['blog-list__item-meta-divider']} />
+              </div>
+              <h2 className={styles['blog-list__item-title']}>
+                {edge.node.frontmatter.title}
+              </h2>
+              {edge.node.frontmatter.tags ? (
+                <ul>
+                  {edge.node.frontmatter.tags.map(tag => {
+                    return <li key={tag}>{tag}</li>;
+                  })}
+                </ul>
+              ) : null}
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BlogList;
