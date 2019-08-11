95df9cd8363eeba7e86f81145dbe21f802f5009e
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-08-11 09:34:39 -0700

    add argument[] to useEffect in Layout and eliminate unnecessary code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,9 @@
 // @flow
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Helmet from 'react-helmet';
 import type { Node as ReactNode } from 'react';
 import { navigate } from 'gatsby';
 import styles from './Layout.module.scss';
-import { LanguageContext, LanguageProvider } from '../../utils/languageContext';
 
 type Props = {
   children: ReactNode,
@@ -13,9 +12,6 @@ type Props = {
 };
 
 const Layout = ({ children, title, description, language }: Props) => {
-  const languageContext = useContext(LanguageContext);
-  const [languageB, setLanguage] = useState(languageContext.language);
-
   useEffect(() => {
     if (!language && !sessionStorage.getItem('visited')) {
       sessionStorage.setItem('visited', 'true');
@@ -30,22 +26,20 @@ const Layout = ({ children, title, description, language }: Props) => {
         }
       }
     }
-  });
+  }, []);
 
   return (
-    <LanguageProvider value={{ languageB, setLanguage }}>
-      <div className={styles.layout}>
-        <Helmet>
-          <html lang="en" />
-          <title>{title}</title>
-          <meta name="description" content={description} />
-          <meta property="og:site_name" content={title} />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
-        </Helmet>
-        {children}
-      </div>
-    </LanguageProvider>
+    <div className={styles.layout}>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+      </Helmet>
+      {children}
+    </div>
   );
 };
 
