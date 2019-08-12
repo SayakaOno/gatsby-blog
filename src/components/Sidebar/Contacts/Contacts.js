e8a33b75585235aa21ff6a9e1a99c92944d342f1
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-08-11 22:33:07 -0700

    add tooltips to contact icons on Sidebar

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -16,13 +16,16 @@ const Contacts = ({ contacts, language }: Props) => (
       {Object.keys(contacts).map(name =>
         !contacts[name] ? null : (
           <li className={styles['contacts__list-item']} key={name}>
+            <span className={styles['contacts__list-item-tooltip']}>
+              {name}
+            </span>
             <a
               className={styles['contacts__list-item-link']}
               href={getContactHref(name, contacts[name][language])}
               rel="noopener noreferrer"
               target="_blank"
             >
-              <Icon name={name} icon={getIcon(name)} />
+              <Icon icon={getIcon(name)} />
             </a>
           </li>
         )
