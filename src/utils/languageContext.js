ce90083c34ffb8cba2c38a246f8ed5d39c735bbe
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-08-19 14:29:34 -0700

    eliminate unnecessary code

--- src/utils/languageContext.js
+++ src/utils/languageContext.js
@@ -1,22 +1,3 @@
-import React from 'react';
-
-// export const detectBrowserLanguage = () => {
-//   let language =
-//     globalThis.window.navigator.language ||
-//     globalThis.window.navigator.userLanguage;
-//   if (language !== 'ja' && language !== 'en') {
-//     language = 'en';
-//   }
-//   return language;
-// };
-
 export const getLanguage = path => {
   return path.includes('/ja') ? 'ja' : 'en';
 };
-
-export const LanguageContext = React.createContext({
-  // language: detectBrowserLanguage(),
-  setLanguage: () => {}
-});
-export const LanguageProvider = LanguageContext.Provider;
-export const LanguageConsumer = LanguageContext.Consumer;
